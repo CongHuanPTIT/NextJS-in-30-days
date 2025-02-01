309c9d1e87e8e1bac1902f4208c4d631badb897a
CongHuanNguyen <nguyenconghuan12i.lmnx@gmail.com>
2025-02-01 21:05:53 +0700

    Day 8-12: React Fundamental + useState

--- Week2-React/my-react-project/src/App.js
+++ Week2-React/my-react-project/src/App.js
@@ -1,14 +1,64 @@
+import { useState } from "react";
 import "./App.css";
+
+/*
+  Day 8-9-10: React Fundamentals
+  - How React works
+  - Ternary Operations
+  - JSX = HTML in JS
+  - Components and Props
+  => F1Car and App
+*/ 
+const F1Car = (props) => {
+  return (
+    <>
+      <h1>Brand: {props.brand}</h1>
+      <h2>Car name: {props.carName}</h2>
+    </>
+  );
+}
 
 const App = () => {
   let name = 'Huan';
   let me = true;
   return (
-    <div className="App">
-      <h1>Hello, React!</h1>
-      <h2>This is {me ? name : 'someone'} coding!</h2>
-    </div>
+    <>
+      <div className="App">
+        <h1>Hello, React!</h1>
+        <h2>This is {me ? name : 'someone'} coding!</h2>
+      </div>
+      <div>
+        <F1Car brand={'Ferrari'} carName={'SF90'}/>
+        <F1Car brand={'Mercedes'} carName={'W20'}/>
+        <F1Car brand={'McLaren'} carName={'MCL34'}/>
+      </div>
+    </>
   );
 }
 
-export default App;
+/*
+  Day 11-12: States management
+  - Use useState() => "Hook" into React states
+  - [stateName, setState] = useState(defaultValue)
+  - Unlike JS, onClick is actually useful here  
+*/
+
+const App2 = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleCounterChange = (delta) => {
+    setCounter((prevCounter) => prevCounter + delta);
+  }
+
+  return (
+    <>
+      <div className="App">
+        <button onClick={() => handleCounterChange(1)}>Up</button>
+        <h1>{counter}</h1>
+        <button onClick={() => handleCounterChange(-1)}>Down</button>
+      </div>
+    </>
+  );
+}
+
+export default App2;
